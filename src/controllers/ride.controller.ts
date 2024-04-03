a3eb3544b3db132cda267dcffb33a67b2d845b09
jefedcreator <jefiene77@gmail.com>
2024-04-03 18:06:50 +0100

    ride controller

---
+++ src/controllers/ride.controller.ts
@@ -0,0 +1,59 @@
+import { CustomApiResponse } from '@utils/functions/apiresponse';
+import { NextFunction, Request, Response } from 'express';
+import RideService from '@services/ride.service';
+import { UserType } from '@models/User.model';
+export default class RideController {
+  private rideService = new RideService();
+
+  createBooking = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let create = await this.rideService.createRide(req.body);
+      return CustomApiResponse(res, 201, 'booking created', create);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  getRideById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let booking = await this.rideService.findRide(req.params.id);
+      return CustomApiResponse(res, 200, 'booking fetched', booking);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  getAllRides = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let bookings = await this.rideService.findAllRides();
+      return CustomApiResponse(res, 200, 'bookings fetched', bookings);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  cancelRide = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let bokingStatus = await this.rideService.cancelRide(
+        req.body.bookingId,
+        req.body.userId,
+        req.body.riderId,
+      );
+      return CustomApiResponse(res, 200, 'booking canceled', bokingStatus);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  approveRide = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let booking = await this.rideService.approveRide(
+        req.body.bookingId,
+        req.body.professionalId,
+      );
+      return CustomApiResponse(res, 200, 'booking accepted', booking);
+    } catch (e) {
+      next(e);
+    }
+  };
+}
